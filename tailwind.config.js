e1102320f2c8f28d2d3e891b4004d6687d2cfcf9
CryptoChris <c.peters@makermaker.de>
2022-01-17 11:37:59 +0100

    removed unused files and added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        colors: {
+            "bubble-gum": "#ff77e9",
+        },
+        extend: {},
+    },
+    plugins: [],
+}
